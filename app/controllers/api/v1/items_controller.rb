b5b3c42685d97f0bd69116c6520bd510c5ad2a8e
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-06 09:58:47 -0700

    Create an item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,6 +4,27 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    render json: Item.find(param[:id])
+    render json: Item.find(params[:id])
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      head :no_content
+    end
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      # head code: "201"
+      render json: item
+    else
+    end
+  end
+
+  private
+    def item_params
+      params.require(:person).permit(:name, :description, :image_url)
+    end
 end
--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -10,6 +10,7 @@ describe 'api/v1/items' do
       expect(response).to be_success
       result = JSON.parse(response.body)
 
+      expect(response.code).to eq("200")
       expect(result[0]["name"]).to eq("chair")
       expect(result[1]["name"]).to eq("table")
     end
@@ -22,7 +23,34 @@ describe 'api/v1/items' do
       expect(response).to be_success
       result = JSON.parse(response.body)
 
-      # expect(result).to eq("chair")
+      expect(response.code).to eq("200")
+      expect(result["name"]).to eq("table")
+      expect(result["description"]).to eq("made of steel")
+      expect(result["image_url"]).to eq("https://i.vimeocdn.com/portrait/58832_300x300")
+      # expect(result). TO NOT HAVE KEY CREATED_AT OR UPDATED_AT
     end
+
+    scenario 'DELETE an item' do
+      item = Item.create(name:"chair", description:"made of wood", image_url:"https://i.vimeocdn.com/portrait/58832_300x300")
+      item2 = Item.create(name:"table", description:"made of steel", image_url:"https://i.vimeocdn.com/portrait/58832_300x300")
+
+      expect(Item.count).to eq(2)
+      delete "/api/v1/items/#{item2.id}"
+
+      expect(response.code).to eq("204")
+      expect(Item.count).to eq(1)
+      expect(Item.last.name).to eq("chair")
+    end
+
+    scenario 'CREATE an item' do
+
+      expect(Item.count).to eq(0)
+      post "/api/v1/items/", item: {name:"bacon", description:"yum", image_url:"https://i.vimeocdn.com/portrait/58832_300x300"}
+
+      expect(Item.count).to eq(1)
+      expect(Item.last.name).to eq("bacon")
+    end
+
+
   end
 end
